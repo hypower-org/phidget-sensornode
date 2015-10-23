e895227b5086c58edfa7b362744fb2cbf0512962
vlnguyen92 <nguyenvanlinh1992@gmail.com>
2015-10-22 23:37:02 -0400

    cleaned up old stuff, using JSON

--- src/edu/hypower/gatech/phidget/PhidgetSensorNode.java
+++ src/edu/hypower/gatech/phidget/PhidgetSensorNode.java
@@ -21,60 +21,38 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  */
 public class PhidgetSensorNode {
 
-	// Stores the raw data as a simple map from sensor name to its floating point value.
+    // Stores the raw data as a simple map from sensor name to its floating point value.
     final ConcurrentHashMap<String,Float> rawDataMap = new ConcurrentHashMap<String, Float>();
 
     public static void main(String[] args){
-    	
-    	/*
-    	 * Sensor Node structure
-    	 */
-    	try {
-			
-        	// 1 - Read in properties file, which is JSON
-        	ObjectMapper mapper = new ObjectMapper();
-    		JsonNode rootNode = mapper.readTree(new File("sensornode.json"));
-			System.out.println("Sensor node IP Address: " + rootNode.get("ip-address"));
-			JsonNode sensorsNode = rootNode.get("sensors");
-			
-			
-        	// 2 - Create a collection of runnables that collect data from the sensors; associate each with the update period value
-        	
-        	// 3 - submit all sensor collecting runnables to the timed executor
-        	
-        	// 4 - establish connection to the server
-        	
-		} catch (JsonProcessingException e) {
-			System.err.println("JSON ERROR: " + e.getMessage());
-			System.exit(-1);
-		} catch (IOException e) {
-			System.err.println("ERROR: " + e.getMessage());
-			System.exit(-1);
-		}
-    	
-    	
-//        SensorProps prop = SensorProps.getInstance();
-//
-//        // Load the properties file into the SensorProps data structure. 
-//        Set<String> propKeys = prop.getAllProperties();
-//        Set<String>[] hashKeys = prop.getProperty(propKeys[0]).split(",");
-//        String[] periods = prop.getProperty(propKeys[1]).split(",");
-//        int numSensors = 0;
-//        for(String key: hashKeys) 
-//        {
-//            if(key != "-1") 
-//            {
-//                numSensors++;
-//
-//
-//            }
-//        }
-            //Set new property in
-            //Verify new property
-            //System.out.println(prop.getAllProperties());
-            //prop.store();
 
-    	System.exit(0);
+        /*
+         * Sensor Node structure
+         */
+        try {
+
+            // 1 - Read in properties file, which is JSON
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode rootNode = mapper.readTree(new File("sensornode.json"));
+            System.out.println("Sensor node IP Address: " + rootNode.get("ip-address"));
+            JsonNode sensorsNode = rootNode.get("sensors");
+
+
+            // 2 - Create a collection of runnables that collect data from the sensors; associate each with the update period value
+
+            // 3 - submit all sensor collecting runnables to the timed executor
+
+            // 4 - establish connection to the server
+
+        } catch (JsonProcessingException e) {
+            System.err.println("JSON ERROR: " + e.getMessage());
+            System.exit(-1);
+        } catch (IOException e) {
+            System.err.println("ERROR: " + e.getMessage());
+            System.exit(-1);
+        }
+
+        System.exit(0);
     }
 
 }
