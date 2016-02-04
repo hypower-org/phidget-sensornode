98a98f9be771e1749c54a94a4f7ca0539a48ff23
Patrick Martin <patrickmartin@fastmail.com>
2016-02-03 22:03:35 -0500

    Added vertx enabled phidget sensor node root file.

---
+++ src/edu/hypower/gatech/phidget/PhidgetSensorNodeVerticle.java
@@ -0,0 +1,22 @@
+package edu.hypower.gatech.phidget;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+
+/**
+ * This class is the root of the vertx enabled phidget sensor node application. 
+ * 
+ * @author pmartin@hsc.edu
+ *
+ */
+public class PhidgetSensorNodeVerticle extends AbstractVerticle {
+
+	@Override
+	public void start(Future<Void> startFuture) throws Exception {
+		// TODO Auto-generated method stub
+		super.start(startFuture);
+	}
+
+	
+	
+}
