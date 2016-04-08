836dee89574b4d89abe687c71c4eabec015f2d94
sam <sam@sheffinet.org>
2016-04-08 17:08:39 -0400

    DataCollectionServerBootstrap

--- src/edu/hypower/gatech/phidget/DataPublisherBootstrap.java
+++ src/edu/hypower/gatech/phidget/DataPublisherBootstrap.java
@@ -7,6 +7,7 @@ import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
+import io.vertx.core.shareddata.LocalMap;
 
 /**
  * Class that spawns the DataCollectionVerticle.
@@ -14,12 +15,15 @@ import io.vertx.core.json.JsonObject;
  *
  */
 public class DataPublisherBootstrap {
+	private static final String address1 = "127.0.0.1.temperature.0";
+	private static final String address2 = "127.0.0.1.humidity.1";
 
 	public static void main(String[] args){
-		
+
 		VertxOptions opt = new VertxOptions()
-							.setWorkerPoolSize(Runtime.getRuntime().availableProcessors());
-		
+				.setWorkerPoolSize(Runtime.getRuntime().availableProcessors());
+
+
 		Handler<AsyncResult<Vertx>> resultHandler = new Handler<AsyncResult<Vertx>>(){
 
 			@Override
@@ -27,27 +31,46 @@ public class DataPublisherBootstrap {
 				if(result.succeeded()){
 					System.out.println("Clustered DataPublisher started.");
 					Vertx vertx = result.result();
-					
+
+
+
+					Long sharedDataTimerId = vertx.setPeriodic(333, new Handler<Long>(){
+						@Override
+						public void handle(Long event) {
+
+							LocalMap<String,Long>  map = vertx.sharedData().getLocalMap("SensorData");
+							map.put("temp", System.currentTimeMillis());
+
+						}
+					});
+
 					Long timerId = vertx.setPeriodic(1000, new Handler<Long>(){
 						@Override
 						public void handle(Long arg0) {
-							JsonObject dataMsg = new JsonObject();
-							dataMsg.put("isLive", true);
-							JsonArray data = new JsonArray();
-							for(int i = 0; i < 10; i++){
-								data.add(Math.random() * i);
-							}
-							dataMsg.put("data", data);
-							vertx.eventBus().publish(ChannelNames.DATA_COLLECTOR_RECV, dataMsg);
+							JsonObject dataMsg1 = new JsonObject();
+							JsonObject dataMsg2 = new JsonObject();
+
+							//dataMsg.put("isLive", true);
+							dataMsg1.put("tempTime", vertx.sharedData().getLocalMap("SensorData").get("temp"));
+							dataMsg2.put("humdTime", vertx.sharedData().getLocalMap("SensorData").get("temp"));
+
+							//JsonArray data = new JsonArray();
+							//for(int i = 0; i < 4; i++){
+							//	data.add(Math.random() * i);
+							//}
+							//dataMsg.put("data", data);
+							//vertx.eventBus().publish(ChannelNames.DATA_COLLECTOR_RECV, dataMsg);
+							vertx.eventBus().publish(address1, dataMsg1);
+							vertx.eventBus().publish(address2, dataMsg2);
 						}
 					});
-					
+
 				}
 				else{
 					System.out.println("Failure: " + result.cause().getMessage());
 				}
 			}
-			
+
 		};
 		Vertx.factory.clusteredVertx(opt, resultHandler);
 	}
--- src/edu/hypower/gatech/phidget/PhidgetSensorNodeVerticle.java
+++ src/edu/hypower/gatech/phidget/PhidgetSensorNodeVerticle.java
@@ -1,7 +1,25 @@
 package edu.hypower.gatech.phidget;
 
-import io.vertx.core.AbstractVerticle;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.phidgets.InterfaceKitPhidget;
+import com.phidgets.PhidgetException;
+import edu.hypower.gatech.phidget.comm.SensorNodeClient;
+import edu.hypower.gatech.phidget.sensor.SensorReader;
+import io.vertx.core.*;
 import io.vertx.core.Future;
+import io.vertx.core.eventbus.EventBus;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.concurrent.*;
 
 /**
  * This class is the root of the vertx enabled phidget sensor node application. 
@@ -11,12 +29,146 @@ import io.vertx.core.Future;
  */
 public class PhidgetSensorNodeVerticle extends AbstractVerticle {
 
-	@Override
-	public void start(Future<Void> startFuture) throws Exception {
+	//@Override
+	//public void start(Future<Void> startFuture) throws Exception {
 		// TODO Auto-generated method stub
-		super.start(startFuture);
+	//	super.start(startFuture);
+//	}
+
+	private final ConcurrentHashMap<String, BlockingQueue<Float>> rawDataMap = new ConcurrentHashMap<String, BlockingQueue<Float>>();
+	// Maps sensors to their associated runnable task.
+	private final HashMap<String, Runnable> sensorUpdateRunners = new HashMap<String, Runnable>();
+	private final HashMap<String, Runnable> sensorClientRunners = new HashMap<String, Runnable>();
+
+	private String nodeIpAddr;
+	private boolean isReady = false;
+
+	private final int TIMEOUT = 5000; // 5s wait time
+
+	public PhidgetSensorNodeVerticle(String pathToConfig) {
+
+		try {
+
+			// Read in properties file, which is JSON
+			ObjectMapper mapper = new ObjectMapper();
+			JsonNode rootNode = mapper.readTree(new File("sensornode.json"));
+			nodeIpAddr = rootNode.get("ip-address").asText();
+			System.out.println("Sensor node IP Address: " + nodeIpAddr);
+			String serverIpAddr = rootNode.get("server-ip-addr").asText();
+			Integer serverPort = rootNode.get("server-port").asInt();
+			System.out.println("Data Collection server IP Address: " + serverIpAddr + ", port " + serverPort);
+
+			System.out.println("Attaching the Interface Kit Phidget...");
+			try {
+				// Phidget initialization
+				InterfaceKitPhidget ikit = new InterfaceKitPhidget();
+				ikit.openAny();
+				ikit.waitForAttachment(TIMEOUT);
+
+
+				System.out.println("complete.");
+				ikit.setRatiometric(true);
+
+
+				ScheduledExecutorService schedExec = Executors
+						.newScheduledThreadPool(Runtime.getRuntime().availableProcessors());
+				ExecutorService exec = Executors.newCachedThreadPool();
+
+				Iterator<Map.Entry<String, JsonNode>> iter = rootNode.get("sensors").fields();
+				while (iter.hasNext()) {
+					Map.Entry<String, JsonNode> entry = (Map.Entry<String, JsonNode>) iter.next();
+					Integer location = entry.getValue().get("location").asInt();
+					Integer updatePeriod = entry.getValue().get("update-period").asInt();
+					String sensorStr = entry.getKey();
+					String sensorKey = sensorStr + "." + location;
+
+					String sensorName = Character.toUpperCase(sensorStr.charAt(0)) + sensorStr.substring(1, sensorStr.length());
+
+					try {
+						Class<?> sensor = Class
+								.forName("edu.hypower.gatech.phidget.sensor." + sensorName + "SensorReader");
+						Constructor<?> cons = sensor.getConstructor(Integer.class, String.class, InterfaceKitPhidget.class, ArrayBlockingQueue.class);
+
+						System.out.println(sensorKey + ", updating every " + updatePeriod + "ms");
+
+						rawDataMap.putIfAbsent(sensorKey, new ArrayBlockingQueue<Float>(1));
+						sensorUpdateRunners.put(sensorKey, (SensorReader) cons.newInstance(location, sensorKey, ikit,
+								(ArrayBlockingQueue<Float>) rawDataMap.get(sensorKey)));
+
+
+						SensorNodeClient client = new SensorNodeClient((ArrayBlockingQueue<Float>) rawDataMap.get(sensorKey),
+								nodeIpAddr, sensorKey, serverIpAddr, serverPort);
+
+						sensorClientRunners.put(sensorKey, client);
+
+						vertx.setPeriodic(updatePeriod, v -> vertx.eventBus().publish(sensorUpdateRunners.get(sensorKey).toString(), sensorClientRunners.get(sensorKey)));
+
+						//schedExec.scheduleAtFixedRate(sensorUpdateRunners.get(sensorKey), 0, updatePeriod, TimeUnit.MILLISECONDS);
+						exec.submit(sensorClientRunners.get(sensorKey));
+
+						isReady = true;
+
+					} catch (ClassNotFoundException cnfe){
+						System.err.println("ERROR: " + sensorName + " not implemented.");
+
+					} catch (NoSuchMethodException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (SecurityException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (InstantiationException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (IllegalAccessException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (IllegalArgumentException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (InvocationTargetException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			} catch (PhidgetException e) {
+				System.err.println("Error: Timeout ( " + TIMEOUT + "ms) reached. No phidget Interface Kit detected.");
+			}
+
+
+		} catch (JsonProcessingException e) {
+			System.err.println("JSON ERROR: " + e.getMessage());
+			System.exit(-1);
+		} catch (IOException e) {
+			System.err.println("ERROR: " + e.getMessage());
+			System.exit(-1);
+		}
+
 	}
 
-	
-	
+	public final ArrayList<String> getSensorNames(){
+		final ArrayList<String> sensors = new ArrayList<String>();
+		for(String s : rawDataMap.keySet()){
+			sensors.add(s);
+		}
+		return sensors;
+	}
+
+	public boolean isReady() {
+		return isReady;
+	}
+
+	public static void main(String[] args) {
+
+		final PhidgetSensorNodeVerticle node = new PhidgetSensorNodeVerticle("");
+		if(node.isReady()){
+			while (true) {
+				try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 }
